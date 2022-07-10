565734488a5b95d354d1deb1786f87d7c7d335b8
Riya Hemani <97747860+riyahemani@users.noreply.github.com>
2022-07-10 16:55:05 +0530

    Update addbooks.py

--- addbooks.py
+++ addbooks.py
@@ -9,11 +9,11 @@ import backend
 def bookRegister():
    
   # Getting the values from the entry boxes.
-    bid = bookInfo1.get()
+    #bid = bookInfo1.get()
     title = bookInfo2.get()
     author = bookInfo3.get()
-    status = bookInfo4.get()
-    status = status.lower()
+    #status = bookInfo4.get()
+    #status = status.lower()
     
     # insertBooks = "insert into "+bookTable+" values('"+bid+"','"+title+"','"+author+"','"+status+"')"
     # Trying to insert the data into the database. If it fails, it will show an error message.
@@ -23,10 +23,10 @@ def bookRegister():
     except:
         messagebox.showinfo("Error","Can't add data into Database")
     
-    print(bid)
+    #print(bid)
     print(title)
     print(author)
-    print(status)
+    #print(status)
 
 
     root.destroy()
@@ -61,36 +61,36 @@ def addBook():
 # Creating a frame and placing it in the window.
 
     labelFrame = Frame(root,bg='black')
-    labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4)
+    labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.3)
         
     # Book ID
     # Creating a label and an entry box.
-    lb1 = Label(labelFrame,text="Book ID : ", bg='black', fg='white')
-    lb1.place(relx=0.05,rely=0.2, relheight=0.08)
+    #lb1 = Label(labelFrame,text="Book ID : ", bg='black', fg='white')
+    #lb1.place(relx=0.05,rely=0.2, relheight=0.08)
         
-    bookInfo1 = Entry(labelFrame)
-    bookInfo1.place(relx=0.3,rely=0.2, relwidth=0.62, relheight=0.08)
+    """bookInfo1 = Entry(labelFrame)
+    bookInfo1.place(relx=0.3,rely=0.2, relwidth=0.62, relheight=0.08)"""
         
     # Title
-    lb2 = Label(labelFrame,text="Title : ", bg='black', fg='white')
-    lb2.place(relx=0.05,rely=0.35, relheight=0.08)
+    lb2 = Label(labelFrame,text="Title : ", bg='black', fg='white',font=(10))
+    lb2.place(relx=0.05,rely=0.35, relheight=0.12)
         
     bookInfo2 = Entry(labelFrame)
-    bookInfo2.place(relx=0.3,rely=0.35, relwidth=0.62, relheight=0.08)
+    bookInfo2.place(relx=0.25,rely=0.35, relwidth=0.62, relheight=0.12)
         
     # Book Author
-    lb3 = Label(labelFrame,text="Author : ", bg='black', fg='white')
-    lb3.place(relx=0.05,rely=0.50, relheight=0.08)
+    lb3 = Label(labelFrame,text="Author : ", bg='black', fg='white', font=(10))
+    lb3.place(relx=0.05,rely=0.60, relheight=0.12)
         
     bookInfo3 = Entry(labelFrame)
-    bookInfo3.place(relx=0.3,rely=0.50, relwidth=0.62, relheight=0.08)
+    bookInfo3.place(relx=0.25,rely=0.60, relwidth=0.62, relheight=0.12)
         
     # Book Status
-    lb4 = Label(labelFrame,text="Status(Avail/issued) : ", bg='black', fg='white')
+    """lb4 = Label(labelFrame,text="Status(Avail/issued) : ", bg='black', fg='white')
     lb4.place(relx=0.05,rely=0.65, relheight=0.08)
         
     bookInfo4 = Entry(labelFrame)
-    bookInfo4.place(relx=0.3,rely=0.65, relwidth=0.62, relheight=0.08)
+    bookInfo4.place(relx=0.3,rely=0.65, relwidth=0.62, relheight=0.08)"""
         
     #Submit Button
     SubmitBtn = Button(root,text="SUBMIT",bg='#d1ccc0', fg='black',command=bookRegister)
